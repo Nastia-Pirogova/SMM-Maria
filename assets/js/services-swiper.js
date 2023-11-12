3ea6c392c020bc46967689c775fb9ab417e823ce
yarikleva <levchenko153729@gmail.com>
2023-11-12 13:12:17 +0300

    section services

---
+++ assets/js/services-swiper.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.s-button-next',
+        prevEl: '.s-button-prev',
+    },
+
+
+});
