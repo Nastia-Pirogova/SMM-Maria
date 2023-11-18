fc83460f4213d698955d39a114bb3ec09b9f6b68
Anastasia Pirogova <109249070+Nastia-Pirogova@users.noreply.github.com>
2023-11-18 22:28:08 +0200

    update

--- assets/js/footer-scroll-up.js
+++
@@ -1,7 +0,0 @@
-const scrollToTop = document.querySelector(".footer-inner__top-button");
-scrollToTop.addEventListener("click", () => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-    });
-});
